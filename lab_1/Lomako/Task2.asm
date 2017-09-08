245e47b47bb49cd5f2609375c45d05b34c940730
Maksim42 <maksim.lomako@gmail.com>
2017-09-08 11:57:10 +0300

    L1T2/3: fix variable name

--- lab_1/Lomako/Task2.asm
+++ lab_1/Lomako/Task2.asm
@@ -6,8 +6,8 @@ c_arLen set     0x20    ; array length
 ; var
 v_i equ         0x29
 v_j equ         0x2A
-v_min equ       0x2B
-v_minpos equ    0x2C
+v_ext equ       0x2B
+v_extpos equ    0x2C
 v_tmp equ       0x2D
 
 BEGIN:
@@ -18,22 +18,22 @@ LOOPI:
     MOVF v_i, 0         ; W = i
     MOVWF v_j           ; j = i
     ADDLW c_arA
-    MOVWF v_minpos      ; v_minpos = a[i].address
+    MOVWF v_extpos      ; v_extpos = a[i].address
     MOVWF FSR           ; min = a[i]
     MOVF INDF, 0
-    MOVWF v_min
+    MOVWF v_ext
     LOOPJ:
         MOVF v_j, 0         ; W = a[j]
         ADDLW c_arA
         MOVWF FSR
         MOVF INDF, 0
-        SUBWF v_min, 0      ; if a[j] < v_min
+        SUBWF v_ext, 0      ; if a[j] < v_ext
         BTFSC STATUS, 0
         GOTO IFEND
             MOVF FSR, 0
-            MOVWF v_minpos      ; maxpos = a[j].address
+            MOVWF v_extpos      ; maxpos = a[j].address
             MOVF INDF, 0        ; max = a[j]
-            MOVWF v_min     
+            MOVWF v_ext
         IFEND:                  ; end if
     DECFSZ v_j, 1
     GOTO LOOPJ          ; end LOOPJ
@@ -42,9 +42,9 @@ LOOPI:
     MOVWF FSR
     MOVF INDF, 0
     MOVWF v_tmp          
-    MOVF v_min, 0       ; a[i] = min
+    MOVF v_ext, 0       ; a[i] = min
     MOVWF INDF
-    MOVF v_minpos, 0    ; a[minpos] = tmp
+    MOVF v_extpos, 0    ; a[minpos] = tmp
     MOVWF FSR
     MOVF v_tmp, 0
     MOVWF INDF
