3e99f7d7431b77d7e2455f976b0011cdc1a14800
Naturebringer <eragonaraizers@mail.ru>
2017-09-28 10:42:12 +0300

    c

--- lab_2/Second.asm
+++ lab_2/Second.asm
@@ -35,16 +35,23 @@ CREATEARRAY: ; работает
 
 WRITE:
 
-	BSF STATUS, RP0 ; Bank 1
-	BCF INTCON, GIE ; Disable INTs.
-	BSF EECON1, WREN ; Enable Write
-	MOVLW 55h ;
+	BSF STATUS, 0x06 ; Bank 1
+	BCF INTCON, 0x07; Disable INTs.
+	BSF EECON1, 0x02 ; Enable Write
+	MOVLW 0x55 ;
 	MOVWF EECON2 ; Write 55h
-	MOVLW AAh ;
+	MOVLW 0xAA ;
 	MOVWF EECON2 ; Write AAh
 	BSF EECON1,0x01 ; Set WR bit; begin write
-	BSF INTCON, GIE ; Enable INTs.
-;	MOVF EEDATA, W
+	BSF INTCON, 0x07 ; Enable INTs.
+
+	MOVF 0x00,0
+	MOVWF EEADR
+	MOVF 0x01,0
+	MOVWF EEDATA
+	BCF EECON1,0x01 
+;	MOVWF EEDATA
+	
 
 	CLRF v_ptr        
 	end
